94c2c945e1426f4848ef3b9fbff2ed2af541a023
Bryan <bryanacuna1994@icloud.com>
2026-01-21 18:52:36 -0500

    projects

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,20 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar">
-        <NavLink to="/" end>Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </nav>
+      <header className="header">
+        <div className="logo">
+          <svg viewBox="0 0 100 100" className="mountain-icon">
+            <polygon points="50,15 85,85 15,85" fill="#646cff" />
+            <polygon points="50,35 70,85 30,85" fill="#535bf2" />
+          </svg>
+          <span className="brand-name">Andariegos</span>
+        </div>
+        <nav className="navbar">
+          <NavLink to="/" end>Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
